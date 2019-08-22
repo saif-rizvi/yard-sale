74478057e84764390803de67d0eb34ba5b86f7e4
Saif Rizvi <saif1229@gmail.com>
2019-08-22 13:38:08 -0400

    Updates

---
+++ precache-manifest.717fa91cc5b248ff53e7d54899870397.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43a4c7f9927f937d6fd6f2447477782",
+    "url": "/yard-sale/index.html"
+  },
+  {
+    "revision": "a110ce7de689b2107591",
+    "url": "/yard-sale/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d35552c8194050484d1d",
+    "url": "/yard-sale/static/js/2.70f43ba6.chunk.js"
+  },
+  {
+    "revision": "a110ce7de689b2107591",
+    "url": "/yard-sale/static/js/main.5fe75329.chunk.js"
+  },
+  {
+    "revision": "dce9319d465932b7a01b",
+    "url": "/yard-sale/static/js/runtime~main.8f5f5653.js"
+  }
+]);
